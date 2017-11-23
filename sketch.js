cb1f4cbbfa5124e0139dbc93c9099352e976e5d4
William Barbosa <williampmb@gmail.com>
2017-11-22 23:32:03 -0300

    - Added PVector

--- sketch.js
+++ sketch.js
@@ -2,9 +2,11 @@
 
 let bubble;
 let bubble2;
+var widthScreen =800;
+var heightScreen = 600;
 
 function setup(){
-	createCanvas(800,600);
+	createCanvas(widthScreen,heightScreen);
 	bubble = new Bubble(200,200,20);
 	bubble2 = new Bubble(400,200,40);
 	print(bubble.x,bubble.y);
@@ -24,22 +26,30 @@ function draw(){
 
 class Bubble{
 	constructor(x,y,r){
-		this.x = x;
-		this.y = y;
+		this.location = new PVector(x,y);
+		this.velocity = new PVector(2,2);
 		this.r = r
 	}
 
 	move(){
-		this.x = this.x + random(-5,5);
-		this.y = this.y + random(-5,5);
+		this.location.axisX = this.location.axisX + this.velocity.axisX;
+		if(this.location.axisX > widthScreen || this.location.axisX  <0) this.velocity.axisX *=-1;
+		this.location.axisY = this.location.axisY + this.velocity.axisY;
+		if(this.location.axisY > heightScreen || this.location.axisY  <0) this.velocity.axisY *=-1;
 	}
 
 	show(){
 		stroke(255);
 		strokeWeight(4);
 		noFill();
-		ellipse(this.x,this.y,this.r*2);
+		ellipse(this.location.axisX,this.location.axisY,this.r*2);
 	}
+}
 
+class PVector{
+	constructor(x,y){
+		this.axisX = x;
+		this.axisY = y;
+	}
 
 }
