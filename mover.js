1cd897d547ec122137048e851a9a323546352ca2
William Barbosa <williampmb@gmail.com>
2017-11-26 16:02:01 -0300

    - acceleration - p5.js api p5.vector

--- mover.js
+++ mover.js
@@ -1,24 +1,39 @@
 class Mover{
   constructor(){
-    this.location = new PVector(3/2, 3/2);
-    this.velocity = new PVector(1,3);
+    // this.location = new PVector(width/2, height/2);
+    // this.velocity = new PVector(width/2,height/2);
+    // this.acceleration = new PVector(0,0);
+
+    //p5.Vector from API
+    this.location = createVector(width/2, height/2);
+    this.velocity = createVector(width/2,height/2);
+    this.acceleration = createVector(0,0);
   }
 
   update(){
-  this.location.add(this.velocity);
+    let mouse = createVector(mouseX,mouseY);
+    mouse.sub(this.location);
+    mouse.setMag(0.1);
+
+    //this.acceleration = p5.Vector.random2D();
+    this.acceleration = mouse;
+
+    this.velocity.add(this.acceleration);
+    this.location.add(this.velocity);
+    this.velocity.limit(5);
   }
 
   edges(){
-    if(this.location.axisX > width)  this.location.axisX  = 0;
-    if(this.location.axisX < 0)      this.location.axisX = width;
-    if(this.location.axisY > height) this.location.axisY = 0;
-    if(this.location.axisY < 0)      this.location.axisY = height;
+    if(this.location.x > width)  this.location.x  = 0;
+    if(this.location.x < 0)      this.location.x = width;
+    if(this.location.y > height) this.location.y = 0;
+    if(this.location.y < 0)      this.location.y = height;
   }
 
   show(){
     stroke(0);
     strokeWeight(2);
     fill(127);
-    ellipse(this.location.axisX, this.location.axisY, 48, 48);
+    ellipse(this.location.x, this.location.y, 48, 48);
   }
 }
